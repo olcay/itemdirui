13305d445b208385d5d2a069ef08e4f74195c629
Olcay Bayram <obayram@suitsupply.com>
2020-09-24 23:43:09 +0200

    Get items

---
+++ src/app/_services/item.service.ts
@@ -0,0 +1,21 @@
+﻿import { Injectable } from '@angular/core';
+
+import { environment } from '@environments/environment';
+import { ItemDirectoryApiClient } from './itemdirapi.client';
+
+const baseUrl = `${environment.apiUrl}`;
+
+@Injectable({ providedIn: 'root' })
+export class ItemService {
+    private client;
+
+    constructor(
+    ) {
+        this.client = new ItemDirectoryApiClient(baseUrl);
+    }
+
+    getClient() {
+        return this.client;
+    }
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,25 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 
-import { AccountService } from '@app/_services';
+import { AccountService, ItemService } from '@app/_services';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
     account = this.accountService.accountValue;
 
-    constructor(private accountService: AccountService) { }
+    private data: any = []
+
+    constructor(private accountService: AccountService, private itemService: ItemService) {
+    }
+
+    getData() {
+        this.itemService.getClient().getItems(undefined, undefined, undefined, undefined, undefined)
+            .then((res) => {
+                console.log(res)
+                this.data = res.value;
+            });
+    }
+
+    ngOnInit() {
+        this.getData()
+    }
 }
